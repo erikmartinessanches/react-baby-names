cc68d773aa57fb752116cec7b06a3732dba3d835
Erik Martines Sanches <erik.martinessanches@gmail.com>
2021-05-03 22:27:11 +0200

    Adding names to short list, removing them from name picker.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, {useState, Fragment} from 'react'
 
 import NamePicker from './components/NamePicker'
 import Search from './components/Search'
+import ShortList from './components/ShortList'
 /** We can either receive 'props' containing all props, or
  * destructure in the argument parenthesis and only receive
  * names property. */
 function App({names}) {
   const [searchValue, setSearchValue] = useState("") //Passing an initial default name...
+  const [shortList, setShortList] = useState([]);
 
   return (
     <Fragment>
       <Search searchValue={searchValue} setSearchValue={setSearchValue}/>
-      <NamePicker names={names} searchValue={searchValue}/>
+      <ShortList shortList={shortList} setShortList={setShortList}/>
+      <NamePicker names={names} searchValue={searchValue} shortList={shortList} setShortList={setShortList}/>
     </Fragment>
     
   );
--- src/components/NamePicker.js
+++ src/components/NamePicker.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-function NamePicker({names, searchValue}) {
-    const filteredNames = names.filter((entry)=>
-        entry.name.toLowerCase().includes(searchValue.toLowerCase())
+function NamePicker({names, searchValue, shortList, setShortList}) {
+    const filteredNames = names
+    .filter((entry)=> //Only show names in the NamePicker containing the value in the search box.
+        entry.name.toLowerCase().includes(searchValue.toLowerCase()) 
     )
+    .filter((entry) => !shortList.includes(entry.id)) //Do not show names in NamePicker that are in ShortList
+
+    function addToShortList(id){
+        setShortList([...shortList, id]) //"Append" id to existing shortList array.
+    }
 
     return (
         <ul>
-            {filteredNames.map(entry => <li className={entry.sex} key={entry.id}><button>{entry.name}</button></li>)}
+            {filteredNames.map(entry => <li className={entry.sex} key={entry.id}><button onClick={()=> addToShortList(entry.id)} >{entry.name}</button></li>)}
         </ul>
     );
 }
